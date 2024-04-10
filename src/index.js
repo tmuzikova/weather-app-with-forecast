b491cc05fcb1b2d1f56f267c23cd6409b49d6eaa
tmuzikova <tereza.muzikova@gmail.com>
2024-04-10 23:13:25 +0200

    api forecast integrated

--- src/index.js
+++ src/index.js
@@ -44,13 +44,17 @@ function refreshWeatherData(response) {
   weatherPicture.src = response.data.condition.icon_url;
 
   refreshDate(response.data.time);
+
+  //dynamic changing of background based on current time and weather
 }
 
 //function to call api
 function searchCity(city) {
   let apiKey = "6ddtf02cf3a61b83b8oaf010eba5f493";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl2 = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeatherData);
+  axios.get(apiUrl2).then(displayForecast);
 }
 //function to update the heading based on the search input
 function handleSearchSubmit(event) {
@@ -60,27 +64,29 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 //function to display forecast
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.getElementById("weather-forecast");
-
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast-day">
+  for (let i = 1; i < 6; i++) {
+    const forecast = response.data.daily[i];
+
+    const date = new Date(forecast.time * 1000);
+    const day = date.toLocaleDateString("en-US", { weekday: "short" });
+    const maxTemperature = Math.round(forecast.temperature.maximum);
+    const minTemperature = Math.round(forecast.temperature.minimum);
+    const iconUrl = forecast.condition.icon_url;
+    forecastHtml += `<div class="weather-forecast-day">
         <div class="weather-forecast-date">${day}</div>
         <img
-          src="https://cdn-icons-png.flaticon.com/128/869/869869.png"
-          width="45"
+          src=${iconUrl} width="60"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max">18°</span>
-          <span class="weather-forecast-temperature-min">12°</span>
+          <span class="weather-forecast-temperature-max">${maxTemperature}</span>
+          <span class="weather-forecast-temperature-min">${minTemperature}</span>
         </div>
       </div>`;
-  });
+  }
 
   forecastElement.innerHTML = forecastHtml;
 }
@@ -89,4 +95,3 @@ let searchFormElement = document.getElementById("search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Prague");
-displayForecast();
